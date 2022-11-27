8e60bc93a413263f10384537ddc23cf3e11ed35e
Dodo <dododawood236@gmail.com>
2022-11-27 22:05:21 +0200

    create islami app

---
+++ android/app/src/main/kotlin/com/example/isalmi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.isalmi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = 'home';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
