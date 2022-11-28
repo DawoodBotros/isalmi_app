12423215d23f56f351a3a8891959d1ea3a7fb0b5
Dodo <dododawood236@gmail.com>
2022-11-28 10:29:58 +0200

    create islami app

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName = 'home';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
